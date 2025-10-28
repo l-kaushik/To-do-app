12414c9419908d54e5c744567cdda95cba7dc876
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2025-10-28 23:19:49 +0530

    Update User.java
    Implemented builder designer pattern to simplify User object creation.
    Replaced table schema to table name, fixed database "table not found error".

--- backend/to-do-app/src/main/java/in/lokeshkaushik/to_do_app/model/User.java
+++ backend/to-do-app/src/main/java/in/lokeshkaushik/to_do_app/model/User.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(schema = "users")
+@Table(name = "users")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User {
 
@@ -38,6 +38,16 @@ public class User {
     @Column(nullable = false)
     private Instant updatedAt;
 
+    private User(Builder builder) {
+        setUsername(builder.username);
+        setEmailId(builder.emailId);
+        setPassword(builder.password);
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
     @PrePersist
     public void onCreate() {
         this.createdAt = Instant.now();
@@ -47,5 +57,33 @@ public class User {
     @PreUpdate
     public void onUpdate() {
         this.updatedAt = Instant.now();
+    }
+
+    public static final class Builder {
+        private String username;
+        private String emailId;
+        private String password;
+
+        private Builder() {
+        }
+
+        public Builder username(String val) {
+            username = val;
+            return this;
+        }
+
+        public Builder emailId(String val) {
+            emailId = val;
+            return this;
+        }
+
+        public Builder password(String val) {
+            password = val;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
     }
 }
